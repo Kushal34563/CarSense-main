ae0e431269062826b52f9ed2a1eff45ebdb896bf
kushal34563 <kushal34563@gmail.com>
2024-06-02 21:55:25 +0530

    test upload

---
+++ update-car-detect-props.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ 
+# Connect to the EC2 instance using SSH with the provided private key and hostname
+ssh -i "aws-project-101-keyPair.pem" ec2-user@ XXXXXXXXXX.compute-1.amazonaws.com <<EOF
+    
+    # Change directory to the specified path
+    cd /home/ec2-user/awsproject/ec2-carRecognizer
+ 
+    # Replace Application.properties
+
+    echo -e "
+    aws.access.key=XXXXXXXXXX
+    aws.secret.key=XXXXXXXXXX
+    aws.session.token=XXXXXXXXXX
+    aws.queue.url=XXXXXXXXXX
+    proj=proj" > src/main/resources/application.properties
+
+    #build the application
+    
+    mvn clean compile assembly:single
+EOF
